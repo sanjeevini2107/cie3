63cbff67377c2357c9628d726370e67200eef0f1
sanjeevini <gotragavissd@gmail.com>
2025-05-14 15:48:07 +0530

    Initial commit

---
+++ BankCustomer.java
@@ -0,0 +1,22 @@
+public class BankCustomer {
+    private String name;
+    private String accountNumber;
+    private int age;
+
+    public BankCustomer(String name, String accountNumber, int age) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.age = age;
+    }
+
+    public void displayCustomerInfo() {
+        System.out.println("Customer Name: " + name);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Customer Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        BankCustomer customer = new BankCustomer("Nisarga", "1234567890", 21);
+        customer.displayCustomerInfo();
+    }
+}
